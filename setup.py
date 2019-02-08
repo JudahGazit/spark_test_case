325a3923335fa6faa48324dc3fa4a989a78277d8
Rotem Gazit <gazitrotem@gmail.com>
2019-02-08 19:29:24 +0200

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='spark_test_case',
+    version='',
+    packages=['spark_test_case'],
+    url='',
+    license='',
+    author='Rotem',
+    author_email='',
+    description=''
+)
